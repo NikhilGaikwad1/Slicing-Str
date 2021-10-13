71dc5ab16b07584d75c0e75a3b8e327f82435c6b
Nikhil Rajabhau Gaikwad <74707869+MrJonathanwick@users.noreply.github.com>
2021-10-13 21:40:59 +0530

    initialcommit

---
+++ slice.py
@@ -0,0 +1,11 @@
+def reverse(string):
+    string = string[::-1]
+    return string
+
+s = "Berserk"
+
+print("Original string is",s)
+
+print ("Reversed string is",reverse(s))
+
+#here we have a reversed a string using string slicing very easily
